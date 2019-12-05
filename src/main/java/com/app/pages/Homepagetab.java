5c0429c3d7add17e360e4427c2d39d403228bbcc
Welcome <Kausalya>
2019-12-05 10:01:35 +0530

    Adding "Homepage", "BasePage" and "Testpage" of tab

---
+++ src/main/java/com/app/pages/Homepagetab.java
@@ -0,0 +1,40 @@
+package com.app.pages;
+import java.util.ArrayList;
+import java.util.List;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
+public class Homepagetab extends Basepagetab{
+	
+	
+	@FindBy(xpath = "//*[@id=\"block_top_menu\"]/ul/li[1]/a")
+	private WebElement Women;
+	
+	@FindBy(xpath = "//*[@id=\"block_top_menu\"]/ul/li[2]/a")
+	private WebElement Dresses;
+	
+	@FindBy(xpath = "//*[@id=\"block_top_menu\"]/ul/li[3]/a")
+	private WebElement Tshirts;
+
+
+	public Homepagetab() {
+		PageFactory.initElements(driver, this);
+	}
+		
+		//Get the tab details in home page//
+		
+	  public WebElement getWomen() {
+		  return Women;
+		  }
+			public WebElement getDresses() {
+				return Dresses;
+	}
+			public WebElement getTshirts() {
+				return Tshirts;
+	}
+
+
+}
+
